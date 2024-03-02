e3f686c13a47cd70c1dfd61fe6faa0b71e72b7f9
Yuseop <dbtjq1592000@gmail.com>
2024-03-02 13:27:29 +0900

    :zap: feat: Season Exception 파일 생성

---
+++ threekingdom/src/main/java/threekingdom/wolf/threekingdom/global/error/exception/SeasonException.java
@@ -0,0 +1,10 @@
+package threekingdom.wolf.threekingdom.global.error.exception;
+
+import threekingdom.wolf.threekingdom.global.error.ErrorCode;
+
+public class SeasonException extends BaseException{
+
+    public SeasonException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+}
